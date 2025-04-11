28b06b276facee975f80f65d788da6995c2772e9
Sohan Rout <sohanrout84@gmail.com>
2025-04-11 12:54:17 +0530

    fixed file issue 1

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,6 @@
 'use client';
 import Navbar from "@/app/components/navbar";
-import Hero from "@/app/components/Hero";
+import Hero from "@/app/components/hero";
 import Feature from "@/app/components/features";
 import Footer from "@/app/components/footer";
 
